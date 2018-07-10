f3256e2e64e357eee2c6d9b7e10da0d74f0fc60b
Renjith-PC\Renjith <renjith@singx.co>
2018-07-10 18:31:48 +0800

    Final commit

--- Solution.java
+++ Solution.java
@@ -1,23 +1,22 @@
 
-
+// All Animal walk
 interface Animal {
 	void walk();
-	void sing();
 }
 
-interface  WaterBirds
+// For All Animals who can Swim
+interface  swimmer
 {
 	void swim();
 }
+//For All Birds who can Fly
 interface  flyingBirds
 {
 	void fly();
 }
-interface  Birdtype
-{
-	void fly();
-}
+
 class Bird implements Animal {
+// Implementing Walking
 	public void walk(){
 		System.out.println("I am walking");
 		}
@@ -25,12 +24,14 @@ class Bird implements Animal {
 public void sing() {
 System.out.println("I am singing");
 }
+//Implementing making sounds
 void say(String words) {
 System.out.println(words);
 }
 }
-class Duck extends Bird implements WaterBirds,flyingBirds
+class Duck extends Bird implements swimmer,flyingBirds
 {
+	// ducks Swim and fly
 	public void swim() {
 		System.out.println("I am Swimming");
 		}
@@ -41,6 +42,8 @@ class Duck extends Bird implements WaterBirds,flyingBirds
 		
 	}
 }
+
+// Rooster is a chicken hence below implementation.
 class Chicken extends Bird{
 	
 	
@@ -54,6 +57,8 @@ class Chicken extends Bird{
 		}
 	
 }
+
+// Finally the main class.
 public class Solution {
 public static void main(String[] args) {
 Bird duck = new Bird();
@@ -61,15 +66,18 @@ Bird chicken = new Bird();
 Bird bird = new Bird();
 bird.walk();
 bird.sing();
+
+// Brids Sound..
 duck.say("Quack, quack");
 chicken.say("Cluck, cluck");
 
+// Ducks swim and fly
 Duck d1=new Duck();
 d1.swim();
 d1.fly();
 
+// Roosters belongs here.
 Chicken rooster=new Chicken();
-
 rooster.say("Male");
 }
 }
