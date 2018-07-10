9603f3b6dca517d21c8635b1033b8feaa4f555fe
Renjith-PC\Renjith <renjith@singx.co>
2018-07-10 17:29:02 +0800

    Base Code
    Signed-off-by: Renjith-PC\Renjith <renjith@singx.co>

--- Solution.java
+++ Solution.java
@@ -1,46 +1,18 @@
-
-
-interface action{  
- void say(String sound);  
- void walk();
-} 
-
-interface waterbird{  
-	 void swim();  
-	} 
-
-interface landbird{  
-	 void swim();  
-	} 
-
-class Bird  implements action {
+class Animal {
+void walk(){
+System.out.println("I am walking");
+}
+}
+class Bird extends Animal {
 void fly() {
 System.out.println("I am flying");
 }
-void sing() {
-System.out.println("I am singing");
-}
-
-@Override
-public void walk(){
-System.out.println("I am walking");
-}
-
-@Override
-public void say(String sound) {
-
-	System.out.println(sound);
-	}
-
 }
 public class Solution {
 public static void main(String[] args) {
-Bird duck = new Bird();
-Bird chicken = new Bird();
 Bird bird = new Bird();
 bird.walk();
+bird.fly();
 bird.sing();
-duck.say("Quack, quack");
-chicken.say("Cluck, cluck");
 }
 }
