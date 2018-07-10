874ad67d554d2803f840f6165a2b9858c650c2ab
Renjith-PC\Renjith <renjith@singx.co>
2018-07-10 17:38:06 +0800

    the Duck and the Chicken to make their own special sound

--- Solution.java
+++ Solution.java
@@ -3,6 +3,7 @@ void walk(){
 System.out.println("I am walking");
 }
 }
+
 class Bird extends Animal {
 void fly() {
 System.out.println("I am flying");
@@ -12,12 +13,22 @@ System.out.println("I am flying");
 void sing() {
 System.out.println("I am singing");
 }
+
+//
+void say(String words) {
+System.out.println(words);
 }
+}
+
 public class Solution {
 public static void main(String[] args) {
+Bird duck = new Bird();
+Bird chicken = new Bird();
 Bird bird = new Bird();
 bird.walk();
 bird.fly();
 bird.sing();
+duck.say("Quack, quack");
+chicken.say("Cluck, cluck");
 }
 }
